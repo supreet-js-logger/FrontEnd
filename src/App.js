c1a7601c6667bbc1dd3054b69864b19653a55d5c
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-18 03:21:56 +0530

    Setting up the base app with login and sign up functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
-import React, { useEffect } from "react";
-// import { Tracker } from "../../logger";
+import React, { useEffect, useState } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Login from "./Login";
+import { Button, Container } from "@material-ui/core";
+import Signup from "./Signup";
+import MainApp from "./MainApp";
 // const mylogger = Tracker();
 
 const App = () => {
-  function randomConsoleLog() {
-    let consoleType = ["log", "info", "warn", "error"];
-    var randomLogType = Math.floor(Math.random() * consoleType.length);
-    // switch (randomLogType) {
-    // case 0:
-    console.log(`this is log`);
-    // break;
-    // case 1:
-    console.info(`this is log`);
-    //   break;
-    // case 2:
-    console.warn(`this is log`);
-    //   break;
-    // case 3:
-    console.error(`this is log`);
-    // break;
-    // }
-  }
-  function getLogCount() {
-    // console.dir(mylogger.getLogData());
-    // alert(mylogger.getLogCount());
-  }
-  function throwRandomError() {
-    throw new Error("random error");
-  }
+  let [isLaoding, setLoadingState] = useState(true);
+  let [token, setToken] = useState(localStorage.getItem("token"));
+  let [user, setUser] = useState(null);
+  let [showLogin, setShowLogin] = useState(false);
+  let [showSignup, setShowSignup] = useState(false);
+  const getLoggedInUserDetails = async () => {
+    let getLoggedInuser = `${process.env.API_URL}/auth/me`;
+    let response = await fetch(getLoggedInuser, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    let data = await response.json();
+    if (data.success) setUser(data.data);
+    setLoadingState(false);
+  };
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    setToken(null);
+  };
+  useEffect(() => {
+    if (token || isLaoding) getLoggedInUserDetails();
+  }, [token]);
+  if (isLaoding) return <CircularProgress />;
+
   return (
     <>
-      <h1>Helloworld React!</h1>
-      <button onClick={() => randomConsoleLog()}>
+      <Container maxWidth={false} fixed={false} disableGutters={true}>
+        <MainApp
+          user={user}
+          setShowLogin={() => {
+            setShowSignup(false);
+            setShowLogin(true);
+          }}
+          setShowSignup={() => {
+            setShowSignup(true);
+            setShowLogin(false);
+          }}
+          logOut={logOut}
+        />
+        <Container>
+          {!user && (
+            <>
+              {showLogin && <Login setToken={setToken} />}
+              {showSignup && <Signup setToken={setToken} />}
+            </>
+          )}
+          {user && JSON.stringify(user)}
+        </Container>
+      </Container>
+
+      {/* <button onClick={() => randomConsoleLog()}>
         Generate random console log
       </button>
       <button onClick={() => throwRandomError()}>Generate random error</button>
-      <button onClick={() => getLogCount()}>Alert log count</button>
+      <button onClick={() => getLogCount()}>Alert log count</button> */}
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 module.exports = {
   // the output bundle won't be optimized for production but suitable for development
   mode: "development",
@@ -21,10 +27,11 @@ module.exports = {
         exclude: /node_modules/,
         // use the babel-loader for transpiling JavaScript to a suitable format
         loader: "babel-loader",
-        options: {
-          // attach the presets to the loader (most projects use .babelrc file instead)
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
+        // options: {
+        //   // attach the presets to the loader (most projects use .babelrc file instead)
+        //   presets: ["@babel/preset-env", "@babel/preset-react"],
+        //   plugins: ["@babel/plugin-transform-async-to-generator"],
+        // },
       },
     ],
   },
@@ -38,5 +45,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new webpack.DefinePlugin(envKeys),
   ],
 };
