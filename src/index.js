b2d3589ce003901da5474e10d4b8b22de7b362ea
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-18 12:30:05 +0530

    adding sentry support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn:
+    "https://9824c833315842f2a5f36628efb84075@o420201.ingest.sentry.io/5343531",
+});
+
 // import {Tracker} from "../../logger/"
 
 // var mylogger = Tracker();
