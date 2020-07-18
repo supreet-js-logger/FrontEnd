66aee15855e941d2d02ed957fe3d67a8281590e9
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-18 18:13:59 +0530

    using thunk for aqsync reducer update

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import userReducer from "./reducers/userReducer";
 
-const store = createStore(userReducer);
+const store = createStore(userReducer, applyMiddleware(thunk));
 
 export default store;
